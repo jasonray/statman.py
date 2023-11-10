ff31954ad728b790538530c734a17b7c60b2f2e2
Jason Ray <jayray.net@gmail.com>
2023-11-10 13:11:08 -0500

    start / stop / read
    Signed-off-by: Jason Ray <jayray.net@gmail.com>

--- src/stopwatch/stopwatch.py
+++ src/stopwatch/stopwatch.py
@@ -1,5 +1,8 @@
+import time
+
 class Stopwatch():
     _name = None
+    _start_time = None
 
     def __init__(self, name=None):
         self._name = name
@@ -11,3 +14,10 @@ class Stopwatch():
     def name(self) -> str:
         return self._name
     
+    def start(self):
+        self._start_time = time.time()
+
+    def read(self):
+        now = time.time()
+        delta = now - self._start_time
+        return delta
--- src/tests/test_stopwatch.py
+++ src/tests/test_stopwatch.py
@@ -1,8 +1,10 @@
 import unittest
+import time
 from src.stopwatch.stopwatch import Stopwatch
 
 
 class TestStopwatch(unittest.TestCase):
+    _accepted_variance = 0.1
 
     def test_create_with_no_params(self):
         stopwatch = Stopwatch()
@@ -23,3 +25,17 @@ class TestStopwatch(unittest.TestCase):
         stopwatch = Stopwatch(name='sw')
         self.assertIsNotNone(stopwatch)
         self.assertEqual(stopwatch.name,'sw')
+
+    def test_start_read_100ms(self):
+        test_time_s = 0.100
+        stopwatch = Stopwatch(name='sw')
+        stopwatch.start()
+        time.sleep(test_time_s)
+        self.assertAlmostEqual(stopwatch.read(), test_time_s, delta=self._accepted_variance)
+        
+    def test_start_read_1s(self):
+        test_time_s = 1
+        stopwatch = Stopwatch(name='sw')
+        stopwatch.start()
+        time.sleep(test_time_s)
+        self.assertAlmostEqual(stopwatch.read(), test_time_s, delta=self._accepted_variance)
