1e6e4aa2b3c1a7a738234aa8144052d79ae13778
Jason Ray <jayray.net@gmail.com>
2023-11-10 12:44:21 -0500

    constructor tests

--- src/tests/test_stopwatch.py
+++ src/tests/test_stopwatch.py
@@ -13,3 +13,13 @@ class TestStopwatch(unittest.TestCase):
         stopwatch = Stopwatch("")
         self.assertIsNotNone(stopwatch)
         self.assertEqual(stopwatch.name,"")
+
+    def test_create_with_name(self):
+        stopwatch = Stopwatch('sw')
+        self.assertIsNotNone(stopwatch)
+        self.assertEqual(stopwatch.name,'sw')
+
+    def test_create_with_name_by_label(self):
+        stopwatch = Stopwatch(name='sw')
+        self.assertIsNotNone(stopwatch)
+        self.assertEqual(stopwatch.name,'sw')
