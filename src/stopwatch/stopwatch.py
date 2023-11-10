5cad49120dca62101aaee18ae21b155d4abf44ef
Jason Ray <jayray.net@gmail.com>
2023-11-10 12:37:58 -0500

    test: check for name

--- src/stopwatch/stopwatch.py
+++ src/stopwatch/stopwatch.py
@@ -2,3 +2,6 @@ class Stopwatch():
 
     def read(self) -> int:
         return 0
+    
+    def name(self) -> str:
+        return None
--- src/tests/test_stopwatch.py
+++ src/tests/test_stopwatch.py
@@ -4,6 +4,7 @@ from src.stopwatch.stopwatch import Stopwatch
 
 class TestStopwatch(unittest.TestCase):
 
-    def test_sample(self):
+    def test_create_with_no_params(self):
         stopwatch = Stopwatch()
-        self.assertEqual(0, 0)
+        self.assertIsNotNone(stopwatch)
+        self.assertEquals(stopwatch.name(),None)
