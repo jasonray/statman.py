2fd939a58ccaec6de6ceae9e9bea038e3f56e1d6
Jason Ray <jayray.net@gmail.com>
2023-11-11 20:17:56 -0500

    perf compare of two time methods
    Signed-off-by: Jason Ray <jayray.net@gmail.com>

---
+++ perf-time-v-perfcounter.py
@@ -0,0 +1,21 @@
+import time
+# from statman.stopwatch import Stopwatch
+# import sys
+# import statmanP.stopwatchM
+# import statmanM
+from statmanP.stopwatchM import StopwatchC
+
+
+for i in range(0, 2):
+    iterations = 100000000
+
+    swt = StopwatchC('time.time', autostart=True)
+    for i in range(0, iterations):
+        time.time()
+    swt.stop()
+    swt.print()
+    swpc = StopwatchC('time.perf_counter', autostart=True)
+    for i in range(0, iterations):
+        time.perf_counter()
+    swpc.stop()
+    swpc.print()
