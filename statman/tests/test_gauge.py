536f0670627ad7f15c5899c5c794aad07dfb1c01
Jason Ray <jayray.net@gmail.com>
2023-11-11 22:09:34 -0500

    gauge value
    Signed-off-by: Jason Ray <jayray.net@gmail.com>

--- statman/tests/test_gauge.py
+++ statman/tests/test_gauge.py
@@ -4,12 +4,16 @@ from statman.gauge import Gauge
 
 class TestGauge(unittest.TestCase):
     def test_create_with_no_params(self):
-        g = Gauge()
-        self.assertIsNotNone(g)
-        self.assertEqual(g.name, None)
+        gauge = Gauge()
+        self.assertIsNotNone(gauge)
+        self.assertEqual(gauge.name, None)
 
     def test_create_with_name(self):
-        g = Gauge('g')
-        self.assertIsNotNone(g)
-        self.assertEqual(g.name, 'g')
+        gauge = Gauge('g')
+        self.assertEqual(gauge.name, 'g')
+
+    def test_create_with_name(self):
+        gauge = Gauge('g')
+        gauge.value = 5
+        self.assertEqual(gauge.value, 5)
 
