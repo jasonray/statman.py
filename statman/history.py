b6f96f32ebfe3daa7448de0909f42679d5875cf1
Jason Ray <jayray.net@gmail.com>
2023-11-17 17:56:40 -0500

    history stats
    Signed-off-by: Jason Ray <jayray.net@gmail.com>

--- statman/history.py
+++ statman/history.py
@@ -1,17 +1,21 @@
 import time
 import datetime
+import statistics
+from functools import reduce
 
 
 class History():
     _data = []
 
     def __init__(self):
-        _data = []
+        print('pre-init count:', self.count())
+        self._data = []
+        print('post-init count:', self.count())
 
     def __str__(self):
         pass
 
-    def append(self, dt:datetime=None , value:float = None) -> str:
+    def append(self, dt: datetime = None, value: float = None) -> str:
         event = self.create_event(dt=dt, value=value)
         self._data.append(event)
 
@@ -21,6 +25,30 @@ class History():
             dt = datetime.datetime.strptime(dt, '%m/%d/%Y %H:%M:%S')
         event['dt'] = dt
         event['value'] = value
+        return event
 
     def count(self):
         return len(self._data)
+
+    def events(self):
+        return self._data
+
+    def values(self):
+        return [event['value'] for event in self.events()]
+
+    def max_value(self):
+        return reduce(lambda x, y: x if x > y else y, self.values())
+
+    def min_value(self):
+        return reduce(lambda x, y: x if x < y else y, self.values())
+
+    def sum_value(self):
+        return reduce(lambda x, y: x + y, self.values())
+
+    def average_value(self):
+        # return self.sum_value() / self.count()
+        return statistics.fmean(self.values())
+
+    def median_value(self):
+        # return self.sum_value() / self.count()
+        return statistics.median(self.values())
--- statman/tests/test_event_history.py
+++ statman/tests/test_event_history.py
@@ -1,21 +1,100 @@
+import time
 import datetime
 import unittest
+import random
+from statman import Statman
 from statman.history import History
 
 
 class TestEventHistory(unittest.TestCase):
 
     def test_history_empty_count(self):
-        history =  History()
-        self.assertEqual( history.count(), 0 )
+        history = History()
+        self.assertEqual(history.count(), 0)
 
     def test_history_count(self):
-        history =  History()
-        history.append( "01/01/2023 11:31:45", 20) 
-        history.append( "01/01/2023 11:32:45", 10) 
-        history.append( "01/01/2023 11:33:45", 30) 
+        history = History()
+        history.append("01/01/2023 11:31:45", 20)
+        history.append("01/01/2023 11:32:45", 10)
+        history.append("01/01/2023 11:33:45", 30)
 
-        self.assertEqual( history.count(), 3 )
+        self.assertEqual(history.count(), 3)
+
+    def test_history_get_events(self):
+        history = History()
+        history.append("01/01/2023 11:31:45", 20)
+        history.append("01/01/2023 11:32:45", 10)
+        history.append("01/01/2023 11:33:45", 30)
+
+        events = history.events()
+        self.assertEqual(events[0]['dt'].year, 2023)
+
+    def test_history_get_values(self):
+        history = History()
+        history.append("01/01/2023 11:31:45", 20)
+        history.append("01/01/2023 11:32:45", 10)
+        history.append("01/01/2023 11:33:45", 30)
+
+        self.assertEqual(history.values()[1], 10)
+
+    def test_history_get_max_value(self):
+        history = History()
+        history.append("01/01/2023 11:31:45", 20)
+        history.append("01/01/2023 11:32:45", 10)
+        history.append("01/01/2023 11:33:45", 30)
+
+        self.assertEqual(history.max_value(), 30)
+
+    def test_history_get_min_value(self):
+        history = History()
+        history.append("01/01/2023 11:31:45", 20)
+        history.append("01/01/2023 11:32:45", 10)
+        history.append("01/01/2023 11:33:45", 30)
+
+        self.assertEqual(history.min_value(), 10)
+
+    def test_history_get_sum_value(self):
+        history = History()
+        history.append("01/01/2023 11:31:45", 20)
+        history.append("01/01/2023 11:32:45", 10)
+        history.append("01/01/2023 11:33:45", 30)
+
+        self.assertEqual(history.sum_value(), 60)
+
+    def test_history_get_ave_value(self):
+        history = History()
+        history.append("01/01/2023 11:31:45", 20)
+        history.append("01/01/2023 11:32:45", 10)
+        history.append("01/01/2023 11:33:45", 60)
+
+        self.assertEqual(history.average_value(), 30)
+
+    def test_history_get_median_value(self):
+        history = History()
+        history.append("01/01/2023 11:31:45", 20)
+        history.append("01/01/2023 11:32:45", 10)
+        history.append("01/01/2023 11:33:45", 60)
+
+        self.assertEqual(history.median_value(), 20)
 
 
 
+    def test_perf(self):
+        history = History()
+
+        number_of_events = 1000000
+        min_value = 100
+        max_value = 200
+        expected_ave = min_value+ ((max_value-min_value) / 2)
+
+        sw = Statman.stopwatch(autostart=True)
+        for i in range(0 , number_of_events):
+            value = random.randint(min_value,max_value)
+            history.append("01/01/2023 11:31:45", value)
+        sw.stop()
+
+        print('time:', sw.value)
+        sw.print()
+        self.assertEqual(history.count() , number_of_events)
+        self.assertEqual(history.average_value(), expected_ave)
+        1/0
