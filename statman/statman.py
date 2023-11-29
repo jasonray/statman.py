83db6661dfaf9bd188c4b660500842a615293db8
Jason Ray <jayray.net@gmail.com>
2023-11-28 21:29:30 -0500

    format
    Signed-off-by: Jason Ray <jayray.net@gmail.com>

--- statman/statman.py
+++ statman/statman.py
@@ -4,9 +4,10 @@ from .calculation import Calculation
 
 _registry = {}
 
+
 class Rate(Calculation):
-    _numerator_metric_name=None
-    _denominator_metric_name=None
+    _numerator_metric_name = None
+    _denominator_metric_name = None
 
     def __init__(self, name=None, numerator_metric_name=None, denominator_metric_name=None):
         super().__init__(name=name, function=lambda: self._numerator_value / self._denominator_value)
@@ -18,8 +19,8 @@ class Rate(Calculation):
         if not name:
             name = '(Rate)'
         value = self.read(precision=2)
-        numerator = round(self._numerator_value,2)
-        denominator = round(self._denominator_value,2)
+        numerator = round(self._numerator_value, 2)
+        denominator = round(self._denominator_value, 2)
         buffer = f'[{name} => {numerator}/{denominator} = {value}]'
         return buffer
 
@@ -30,7 +31,8 @@ class Rate(Calculation):
     @property
     def _denominator_value(self):
         return Statman.get(self._denominator_metric_name).value
-    
+
+
 class Statman():
 
     def __init__(self):
@@ -86,7 +88,7 @@ class Statman():
         return c
 
     @staticmethod
-    def rate(name=None,  numerator_metric_name=None, denominator_metric_name=None) -> Rate:
+    def rate(name=None, numerator_metric_name=None, denominator_metric_name=None) -> Rate:
         ''' Returns a numeric rate calculation instance.  If there is a registered metric with this name, return it.  If there is no registered metric with this name, create a new instance, register it, and return it. '''
         r = Statman.get(name)
 
