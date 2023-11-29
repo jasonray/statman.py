49262223940f7a30be0a27a64872c5097c081039
Jason Ray <jayray.net@gmail.com>
2023-11-28 21:28:18 -0500

    create a rate calculation metric
    Signed-off-by: Jason Ray <jayray.net@gmail.com>

--- statman/statman.py
+++ statman/statman.py
@@ -4,7 +4,33 @@ from .calculation import Calculation
 
 _registry = {}
 
+class Rate(Calculation):
+    _numerator_metric_name=None
+    _denominator_metric_name=None
 
+    def __init__(self, name=None, numerator_metric_name=None, denominator_metric_name=None):
+        super().__init__(name=name, function=lambda: self._numerator_value / self._denominator_value)
+        self._numerator_metric_name = numerator_metric_name
+        self._denominator_metric_name = denominator_metric_name
+
+    def __str__(self):
+        name = self.name
+        if not name:
+            name = '(Rate)'
+        value = self.read(precision=2)
+        numerator = round(self._numerator_value,2)
+        denominator = round(self._denominator_value,2)
+        buffer = f'[{name} => {numerator}/{denominator} = {value}]'
+        return buffer
+
+    @property
+    def _numerator_value(self):
+        return Statman.get(self._numerator_metric_name).value
+
+    @property
+    def _denominator_value(self):
+        return Statman.get(self._denominator_metric_name).value
+    
 class Statman():
 
     def __init__(self):
@@ -58,6 +84,19 @@ class Statman():
             Statman.register(name, c)
 
         return c
+
+    @staticmethod
+    def rate(name=None,  numerator_metric_name=None, denominator_metric_name=None) -> Rate:
+        ''' Returns a numeric rate calculation instance.  If there is a registered metric with this name, return it.  If there is no registered metric with this name, create a new instance, register it, and return it. '''
+        r = Statman.get(name)
+
+        if not r:
+            r = Rate(name=None, numerator_metric_name=None, denominator_metric_name=None)
+
+        if not name is None:
+            Statman.register(name, r)
+
+        return r
 
     @staticmethod
     def register(name, metric):
---
+++ statman/tests/test_rate.py
@@ -0,0 +1,35 @@
+import unittest
+import time
+from statman import Statman
+from statman.statman import Rate
+from statman.calculation import Calculation
+
+
+class TestRate(unittest.TestCase):
+    _accepted_variance = 0.1
+
+    def test_create(self):
+        rate = Rate()
+        self.assertIsNotNone(rate)
+        self.assertEqual(rate.name, None)
+
+    def test_rate(self):
+        Statman.stopwatch('sw').start()
+        time.sleep(0.5)
+        Statman.stopwatch('sw').stop()
+
+        Statman.gauge('messages_processed').value = 100
+
+        messages_per_second = Rate(name='messages_per_second', numerator_metric_name='messages_processed', denominator_metric_name='sw')
+        self.assertAlmostEqual(messages_per_second.value, 200, delta=5)
+
+    def test_rate_report(self):
+        Statman.stopwatch('sw').start()
+        time.sleep(0.5)
+        Statman.stopwatch('sw').stop()
+
+        Statman.gauge('messages_processed').value = 100
+
+        messages_per_second = Rate(name='messages_per_second', numerator_metric_name='messages_processed', denominator_metric_name='sw')
+
+        messages_per_second.report(output_stdout=True)
