804dfe0520f3bf8432de7395a064fea75ddd6285
Jason Ray <jayray.net@gmail.com>
2023-12-15 10:50:36 -0500

    threadsafe decorator
    Signed-off-by: Jason Ray <jayray.net@gmail.com>

--- statman/decorators.py
+++ statman/decorators.py
@@ -6,7 +6,7 @@ def timer(name):
     def timer_inner_decorator(func):
 
         def timer_wrapper(*args, **kwargs):
-            sw = Statman.stopwatch(name=name, enable_history=True)
+            sw = Statman.stopwatch(name=name, enable_history=True, thread_safe=True)
             sw.start()
             result = func(*args, **kwargs)
             sw.stop()
